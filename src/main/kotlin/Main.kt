8f3499a59a898471f4009952cb6186c5be8d5e5e
attilatolnai <attila.tolnai@student.ehb.be>
2022-05-02 12:05:46 +0200

    Part two of excercise 1 done
    The second part of excercise 1 is done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,34 @@
 fun main(args: Array<String>) {
     val reader = FileReader();
-
     //println(reader.getPuzzle1())
     //println(reader.getPuzzle2())
     //println(reader.getPuzzle3())
     //println(reader.getPuzzle4Numbers())
     //println(reader.getPuzzle4Cards())
-    oneA()
+    //oneA()
+    oneB()
 }
 
-fun oneA() {
+//fun oneA() {
+//    val getFile = FileReader();
+//    val numbers = getFile.getPuzzle1()
+//
+//    for (number in numbers){
+//        println(number)
+//    }
+//
+//    var previousNumber = numbers[0]
+//    var increased = 0;
+//    numbers.forEach {
+//        if (it > previousNumber){
+//            increased++
+//        }
+//        previousNumber = it;
+//    }
+//    println(increased)
+//}
+
+fun oneB() {
     val getFile = FileReader();
     val numbers = getFile.getPuzzle1()
 
@@ -17,15 +36,26 @@ fun oneA() {
         println(number)
     }
 
-    var previousNumber = numbers[0]
+    var counter = 0;
+    numbers.forEachIndexed{ index, i ->
+        if (index < numbers.size -3){
+            var sumA = numbers[index] + numbers[index + 1] + numbers[index + 2]
+            var sumB = numbers[index + 1] + numbers[index + 2] + numbers[index + 3]
+            if (sumB > sumA){
+                counter++
+            }
+        }
+    }
+    var prevInput = numbers[0]
     var increased = 0;
-    numbers.forEach {
-        if (it > previousNumber){
+    numbers.forEach{
+        if (it > prevInput){
             increased++
         }
-        previousNumber = it;
+        prevInput = it;
     }
-    println(increased)
+    println(counter)
 }
+
 
 
