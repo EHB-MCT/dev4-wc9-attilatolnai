d202128d7bcca7defb7d8b30852c13c4ed52632f
attilatolnai <attila.tolnai@student.ehb.be>
2022-05-02 11:48:36 +0200

    First part done
    The first part of the first excersice is done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,31 @@
 fun main(args: Array<String>) {
     val reader = FileReader();
 
-    println(reader.getPuzzle1())
+    //println(reader.getPuzzle1())
     //println(reader.getPuzzle2())
     //println(reader.getPuzzle3())
     //println(reader.getPuzzle4Numbers())
     //println(reader.getPuzzle4Cards())
+    oneA()
 }
+
+fun oneA() {
+    val getFile = FileReader();
+    val numbers = getFile.getPuzzle1()
+
+    for (number in numbers){
+        println(number)
+    }
+
+    var previousNumber = numbers[0]
+    var increased = 0;
+    numbers.forEach {
+        if (it > previousNumber){
+            increased++
+        }
+        previousNumber = it;
+    }
+    println(increased)
+}
+
+
